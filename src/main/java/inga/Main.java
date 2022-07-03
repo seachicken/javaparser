8d958cc5d27a5748b339b35f2b2fe36c6389eed1
seito.tanaka <seito.tanaka.dev@gmail.com>
2022-07-03 19:44:16 +0900

    Read path from stdin

--- src/main/java/inga/Main.java
+++ src/main/java/inga/Main.java
@@ -5,18 +5,29 @@ import com.github.javaparser.serialization.JavaParserJsonSerializer;
 
 import javax.json.Json;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            new JavaParser()
-                    .parse(Paths.get(args[0]))
-                    .getResult()
-                    .ifPresent(ast -> new JavaParserJsonSerializer()
-                            .serialize(ast, Json.createGenerator(System.out)));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        JavaParser javaParser = new JavaParser();
+        JavaParserJsonSerializer jsonSerializer = new JavaParserJsonSerializer();
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String path = scanner.nextLine();
+            try (var writer = new StringWriter();
+                 var jsonGenerator = Json.createGenerator(writer)) {
+                javaParser.parse(Paths.get(path))
+                        .getResult()
+                        .ifPresent(ast -> {
+                            jsonSerializer.serialize(ast, jsonGenerator);
+                            System.out.println(writer);
+                        });
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(-1);
+            }
         }
     }
 }
