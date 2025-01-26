3e65e19e2c81485036bd32f14f17c336d4d58d57
seito.tanaka <seito.tanaka.dev@gmail.com>
2025-01-26 10:00:49 +0900

    Refactor

--- src/main/java/inga/Main.java
+++ src/main/java/inga/Main.java
@@ -6,8 +6,6 @@ import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Scanner;
 
-import static java.util.Collections.emptyList;
-
 public class Main {
     public static void main(String[] args) {
         var parser = new Parser();
